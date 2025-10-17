1e17e6f5dcd9670e7b8aa46549e9bc784775758f
ajintong-15 <allinsjordanintong15@gmail.com>
2025-10-17 14:40:41 +0800

    Add AddRecipesData.js file

---
+++ AddRecipesData.js
@@ -0,0 +1,33 @@
+export const recipesData = [
+  {
+    id: 1,
+    title: "🍝 Creamy Carbonara",
+    image: "https://images.unsplash.com/photo-1603133872878-684f208fb84b",
+    description: "Classic Italian pasta with creamy sauce and bacon.",
+    ingredients: [
+      "200g spaghetti",
+      "100g bacon",
+      "2 eggs",
+      "½ cup Parmesan cheese",
+      "Salt & pepper",
+    ],
+    steps:
+      "1. Cook pasta.\n" +
+      "2. Fry bacon until crisp.\n" +
+      "3. Beat eggs and mix with Parmesan.\n" +
+      "4. Drain pasta and combine with bacon and egg-cheese mixture off heat.\n" +
+      "5. Season to taste and serve immediately.",
+  },
+  {
+    id: 2,
+    title: "🥑 Avocado Toast",
+    image: "https://images.unsplash.com/photo-1555939594-58d7cb561ad1",
+    description: "Crispy toast topped with smashed avocado and eggs.",
+    ingredients: ["2 slices bread", "1 ripe avocado", "1 egg", "Salt & pepper"],
+    steps:
+      "1. Toast the bread slices.\n" +
+      "2. Cut and smash the avocado.\n" +
+      "3. Fry the egg (sunny-side up or as you like).\n" +
+      "4. Spread avocado on toast, place egg on top, season and enjoy.",
+  },
+];
